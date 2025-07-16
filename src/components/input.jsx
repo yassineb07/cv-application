7720273918bcc9ecebee16975857661b1030d041
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-07-16 16:43:36 +0100

    fix: update input component

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -2,7 +2,7 @@ const Input = ({ name, type = 'text', label, onChange }) => {
   return (
     <div>
       <label htmlFor={name}>{label}</label>
-      <input type={type} name={name} onChange={onChange} />
+      <input id={name} type={type} name={name} onChange={onChange} />
     </div>
   );
 };
