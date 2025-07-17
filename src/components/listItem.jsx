d587aabf61c6ee6047867ad88810aac9499a11a0
yassine <98836474+yassineb07@users.noreply.github.com.>
2025-07-17 14:16:49 +0100

    fix: remove console log

--- src/components/listItem.jsx
+++ src/components/listItem.jsx
@@ -1,5 +1,4 @@
 const ListItem = ({ id, name, isActive, onShow, children }) => {
-  console.log({ id, name, isActive, onShow, children });
   return (
     <div id={id} onClick={onShow}>
       {isActive ? children : <h2>{name}</h2>}
